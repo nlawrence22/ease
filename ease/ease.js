5ae0a2b6ace8b0a92b19144bdc84b848aea090d2
nlawrence22 <nlawrence22@users.noreply.github.com>
2015-12-05 17:21:19 -0800

    Fixing for Merge Conflict

--- ease/ease.js
+++ ease/ease.js
@@ -1,36 +1,17 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  /*Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });*/
 
   Template.easecheckin.events({
-      'submit .feelings': function (event) {
-
-          event.preventDefault();
-
-          //Check values of sliders
-          var physical = event.target.physical.value;
-          var emotional = event.target.emotional.value;
-
-          console.log(physical);
-          console.log(emotional);
-
-          //Log values to DB
-          //TODO: Add db code
-      }
+    'submit form': function(){
+      event.preventDefault();
+      var physicalv = event.target.physical.value;
+      var emotionalv = event.target.emotional.value;
+      console.log("Form submitted");
+      console.log(physicalv);
+      console.log(emotionalv);
+        // code goes here
+    }
   });
+
 }
 
 if (Meteor.isServer) {
